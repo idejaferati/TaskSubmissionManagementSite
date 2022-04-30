6f8e6d92e17c6c41bb1d7a709ed24612f89e52c7
MelosYmeri <94323012+MelosYmeri@users.noreply.github.com>
2022-04-30 23:33:31 +0200

    Login_Register

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php 
+
+// $server = "localhost";
+// $user = "root";
+// $pass = "12345678";
+// $database = "login_register";
+
+// $conn = mysqli_connect($server, $user, $pass, $database);
+
+// if (!$conn) {
+//     die("<script>alert('Connection Failed.')</script>");
+// }
+
+
+// $conn = mysqli_connect($server,$user,$pass,$database);
+// if(mysqli_connect_error()){
+//    echo "Failed to connect!";
+//    exit();
+// }
+// echo "Connection success!";
+
+// echo "<br>";
+
+
+
+?>
