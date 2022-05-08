8b03955c890847112cf83acfaf0ef0fad3fbd015
Valmira Shala <valmirashalaa@gmail.com>
2022-05-08 14:05:56 +0200

    worked on registration&login form

---
+++ dbconfig/studentRegistration.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once 'config.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+
+    $userFname      = $_POST['fname'];
+    $userLname      = $_POST['lname'];
+    $userRegNumber  = $_POST['regNumber'];
+    $userName       = $_POST['username'];
+    $email          = $_POST['email'];
+    $userPassword   = $_POST['password'];
+    $register       = $_POST['student_register'];
+    $role="student";
+    $sql=" INSERT INTO users(email,id,name,password,role,surname,username) VALUES ('$email','$userRegNumber','$userFname','$userPassword','$role','$userLname','$userName')";
+    if (mysqli_query($conn, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+
+     mysqli_close($conn);
+}
+?>
+
+
+
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task Management App</title>
+
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body id="home">
+
+    <section id="homePage">
+        <header>
+            <img src="photo/uni_logo.png" alt="Logoja e Universitetit te Prishtines" style="width: 150px;">
+            <h1>Universiteti i Prishtines</h1>
+        </header>
+
+        <div>
+            <h2>Online Assignment Management system</h2>
+        </div>
+
+    
+        <section id="reg_buttons">
+            <a href="lecturer.php"><button>Register as a lecturer</button></a>
+            <a href="student.php"><button>Register as a student</button></a>
+        </section>
+
+        <section>
+            <h3>Already have an account?</h3>
+            <a href="login.php"><button>Login</button></a>
+        </section>
+    </section>
+    
+</body>
+</html>
